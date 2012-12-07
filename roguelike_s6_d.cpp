b55f2f5c1829606f14e53ba8f09d01d81946b10f
luke Frase <totalvideopro@gmail.com>
2012-12-06 22:46:08 -0800

    update mapfile

--- roguelike_s6_d.cpp
+++ roguelike_s6_d.cpp
@@ -772,39 +772,31 @@ void displayOptions()
   }
 
 int openMapFile( char fileName[], short mapArr[][ MAX_COL ] )
-   {
-    // initialize variables
-       ifstream inf;
-       int mapSize = 0, rowIndex = 0;
+{
+  ifstream inf;
+  int x, y;
+  int index, index_2;
 
-    // clear file
-       // function: inf.clear
-          inf.clear();
+  inf.clear();
+  inf.open( fileName );
 
-    // open file
-       // function: inf.open
-          inf.open( fileName );
+  for( index = 0; index < GAME_MAX_X; index++ )
+  {
+    for( index_2 = 0; index_2 < GAME_MAX_Y; index_2++ )
+    {
+      mapArr[index][index_2] = SPACE;
+    }
+  }
 
-    // extract data to map array
-       // function: while
 
-          while( inf.good() )
-            {     
-              // get next character, place in array
-                // function: inf
-                  inf >> mapArr [ rowIndex ][COLUMN_ONE] >> mapArr [ rowIndex ][COLUMN_TWO];
-                  rowIndex++;
-                  mapSize++;
-                  mapSize++;
-            }         
-          
-    // close file
-       // function: inf.close
-          inf.close();
+  while( inf.peak() != EOF )
+  {
+    inf >> x >> y;
 
-    // return the size of the map
-       return mapSize;
-   }
+    mapArr[x][y] = WALL_SYMBOL;
+  }
+  return 0;
+}
 
 void displayMapError()
    {
